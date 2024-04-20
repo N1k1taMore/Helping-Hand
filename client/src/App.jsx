e1d50cc40cfdfd87c8b9d54283be568b714fa1a2
Nikita More <morenikita9670@.com>
2024-04-20 10:24:54 +0530

    Final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,8 +15,6 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import Pay from "./pages/pay/Pay";
-import Success from "./pages/success/Success";
 function App() {
   const queryClient = new QueryClient();
 
@@ -68,14 +66,6 @@ function App() {
         {
           path: "/login",
           element: <Login />,
-        },
-        {
-          path: "/pay/:id",
-          element: <Pay />,
-        },
-        {
-          path: "/success",
-          element: <Success />,
         },
       ],
     },
